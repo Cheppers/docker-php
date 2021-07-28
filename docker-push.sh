c0a545becfd531de6a794a63c395b1e6f890c13a
janost <janost@users.noreply.github.com>
2021-07-28 11:01:18 +0200

    Add php 8.0 image

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGES=(php-5.6 php-7.1 php-7.2 php-7.3 php-7.4)
+IMAGES=(php-5.6 php-7.1 php-7.2 php-7.3 php-7.4 php-8.0)
 HUBUSER="cheppers"
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
